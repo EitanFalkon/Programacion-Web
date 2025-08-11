c728cc950bcbccb8ebf634f6b4a287a54b8cc82b
Eitan <efalkon@itba.edu.ar>
2025-08-11 20:46:38 -0300

    priemr commit

---
+++ clase1.js
@@ -0,0 +1,46 @@
+
+
+let nombre = "Eitan" // string
+let edad="2" // numero == int
+let = true // booleano
+console.log(nombre, edad) // Es igual a al print
+
+// el + tiene una doble funcion, suma si es que puede, 
+// sino, concatena. Lo mismo con la resta
+
+// Para convertir en comentario selecciono y Alt+Shft+A 
+
+// Variables constantes (no cambian de valor) se escriben en mayuscula
+// y se escribe como const Nombre= X
+const constante=33
+
+// ARRAY, LISTA, VECTORES
+
+let array1=[1,2,3] // Arrancan en 0
+console.log(array1[0]) // Printea el 1
+
+// Los arrays se crean con const y en mayus por convencion
+// el .push es el .append para sumar cosas al array
+// Pero si estan en constantes no se pueden sumar
+
+// Una DEF es una function
+// Un objeto es una variable dentro de una funcion
+// Accedo a los objetos de las funciones como ==> NombreFuncion.Objeto
+
+
+// OPERADORES BOOLEANOS
+// Cuando hay dos && es un operador booleano de AND
+// Cuando hay dos || es un operador booleano de OR
+
+// CUENTAS
+// La division con una sola / me devuelve el Cociente de una division
+// El resto de la div me lo da el %
+// Suma y resta es con su simbolo + -
+// Multip es con * y potencia con doble **
+// "2"==2 da True porque es una igualdad debil ==> compara el dato en si
+// "2"===2 da False porque es igualdad fuerte y compara tipop de dato tambien
+// != Diferencia debil (mismo razonamiento que la igualdad)
+// !== Diferencia fuerte (mismo razonamiento que la igualdad)
+
+// CONVERTIR NUMNEROS
+// Se hace con un parceInt(variable)
